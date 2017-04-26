0b29009ef29b4032d661f94378108e40ff0c3029
Adrian Szlegel <adrian95999@hotmail.com>
2017-04-26 17:00:11 +0200

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,85 +1,118 @@
-// Get all the keys from document
-var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '÷'];
-var decimalAdded = false;
+window.onload = function() {
 
-// Add onclick event to all the keys and perform operations
-for(var i = 0; i < keys.length; i++) {
-	keys[i].onclick = function(e) {
-		// Get the input and button values
-		var input = document.querySelector('.screen');
-		var inputVal = input.innerHTML;
-		var btnVal = this.innerHTML;
-		
-		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
-		// If clear key is pressed, erase everything
-		if(btnVal == 'C') {
-			input.innerHTML = '';
-			decimalAdded = false;
-		}
-		
-		// If eval key is pressed, calculate and display the result
-		else if(btnVal == '=') {
-			var equation = inputVal;
-			var lastChar = equation[equation.length - 1];
-			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-			
-			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
-			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
-				equation = equation.replace(/.$/, '');
-			
-			if(equation)
-				input.innerHTML = eval(equation);
-				
-			decimalAdded = false;
-		}
-		
-		// Basic functionality of the calculator is complete. But there are some problems like 
-		// 1. No two operators should be added consecutively.
-		// 2. The equation shouldn't start from an operator except minus
-		// 3. not more than 1 decimal should be there in a number
-		
-		// We'll fix these issues using some simple checks
-		
-		// indexOf works only in IE9+
-		else if(operators.indexOf(btnVal) > -1) {
-			// Operator is clicked
-			// Get the last character from the equation
-			var lastChar = inputVal[inputVal.length - 1];
-			
-			// Only add operator if input is not empty and there is no operator at the last
-			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
-				input.innerHTML += btnVal;
-			
-			// Allow minus if the string is empty
-			else if(inputVal == '' && btnVal == '-') 
-				input.innerHTML += btnVal;
-			
-			// Replace the last operator (if exists) with the newly pressed operator
-			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
-				input.innerHTML = inputVal.replace(/.$/, btnVal);
-			}
-			
-			decimalAdded =false;
-		}
-		
-		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
-		else if(btnVal == '.') {
-			if(!decimalAdded) {
-				input.innerHTML += btnVal;
-				decimalAdded = true;
-			}
-		}
-		
-		// if any other key is pressed, just append it
-		else {
-			input.innerHTML += btnVal;
-		}
-		
-		// prevent page jumps
-		e.preventDefault();
-	} 
+var current,
+    screen,
+    output,
+    limit,
+    zero,
+    period,
+    operator;
+    
+    screen = document.getElementById("result");
+
+var elem = document.querySelectorAll(".num");
+    
+      var len = elem.length;
+    
+      for(var i = 0; i < len; i++ ) {
+        
+        elem[i].addEventListener("click",function() {
+                  
+            num = this.value;
+                     
+            output = screen.innerHTML +=num;
+                  
+            limit = output.length;
+         
+         if(limit > 16 ) {
+        
+         alert("Sorry no more input is allowed");
+             
+       }
+       
+     },false);
+        
+    } 
+
+    document.querySelector(".zero").addEventListener("click",function() {
+        
+        zero = this.value;
+        
+        if(screen.innerHTML === "") {
+            
+           output = screen.innerHTML = zero;  
+        }
+        
+        else if(screen.innerHTML === output) {
+            
+         output = screen.innerHTML +=zero;
+            
+        }
+          
+    },false);
+    
+    document.querySelector(".period").addEventListener("click",function() {
+        
+        period = this.value;
+        
+        if(screen.innerHTML === "") {
+            
+         output = screen.innerHTML = screen.innerHTML.concat("0.");
+            
+         }
+    
+        else if(screen.innerHTML === output) {
+        
+          screen.innerHTML = screen.innerHTML.concat(".");
+            
+        }
+        
+    },false);
+    
+    
+    document.querySelector("#eqn-bg").addEventListener("click",function() {
+        
+      if(screen.innerHTML === output) {
+          
+        screen.innerHTML = eval(output);
+      }
+        
+      else {
+            screen.innerHTML = "";
+      }
+          
+    },false);
+    
+ document.querySelector("#delete").addEventListener("click",function() {
+        
+        screen.innerHTML = "";
+        
+    },false);
+    
+   
+     var elem1 = document.querySelectorAll(".operator");
+    
+      var len1 = elem1.length;
+    
+      for(var i = 0; i < len1; i++ ) {
+        
+        elem1[i].addEventListener("click",function() {
+         
+        operator = this.value;
+         
+         if(screen.innerHTML === "") {
+            
+            screen.innerHTML = screen.innerHTML.concat("");
+            
+        }
+        
+        else if(output) {
+        
+            screen.innerHTML = output.concat(operator);
+            
+        }
+           
+    },false);
+          
+      }   
 }
